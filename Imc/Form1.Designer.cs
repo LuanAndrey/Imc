64e75ddd89bd9d1ee5a250a5a465530203582cad
Luan Andrey <luandrey.99@hotmail.com>
2023-10-30 19:11:37 -0300

    Initial commit

---
+++ Imc/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Imc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txbPeso = new System.Windows.Forms.TextBox();
+            this.txbAltura = new System.Windows.Forms.TextBox();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblClassificacao = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txbPeso
+            // 
+            this.txbPeso.Location = new System.Drawing.Point(93, 97);
+            this.txbPeso.Name = "txbPeso";
+            this.txbPeso.Size = new System.Drawing.Size(100, 20);
+            this.txbPeso.TabIndex = 0;
+            this.txbPeso.TextChanged += new System.EventHandler(this.txbAltura_TextChanged);
+            this.txbPeso.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbAltura_KeyPress);
+            // 
+            // txbAltura
+            // 
+            this.txbAltura.Location = new System.Drawing.Point(93, 143);
+            this.txbAltura.Name = "txbAltura";
+            this.txbAltura.Size = new System.Drawing.Size(100, 20);
+            this.txbAltura.TabIndex = 1;
+            this.txbAltura.TextChanged += new System.EventHandler(this.txbAltura_TextChanged);
+            this.txbAltura.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbAltura_KeyPress);
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Location = new System.Drawing.Point(40, 104);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(53, 13);
+            this.lblPeso.TabIndex = 2;
+            this.lblPeso.Text = "Peso(Kg):";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(40, 150);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(52, 13);
+            this.lblAltura.TabIndex = 3;
+            this.lblAltura.Text = "Altura(M):";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(111, 241);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 4;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(103, 194);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "Calcular IMC";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblClassificacao
+            // 
+            this.lblClassificacao.AutoSize = true;
+            this.lblClassificacao.Location = new System.Drawing.Point(93, 282);
+            this.lblClassificacao.Name = "lblClassificacao";
+            this.lblClassificacao.Size = new System.Drawing.Size(0, 13);
+            this.lblClassificacao.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 388);
+            this.Controls.Add(this.lblClassificacao);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.txbAltura);
+            this.Controls.Add(this.txbPeso);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Calculadora de IMC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbPeso;
+        private System.Windows.Forms.TextBox txbAltura;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblClassificacao;
+    }
+}
+
---
+++ Imc/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Imc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double peso = double.Parse(txbPeso.Text);
+            double altura = double.Parse(txbAltura.Text);
+            double imc = peso / (altura * altura);
+            lblResultado.Text = "IMC:" + imc.ToString("N2");
+
+            if (imc < 18.5)
+            {
+                lblClassificacao.Text = "Abaixo do peso";
+            }
+            else if (imc >= 18.6 && imc <= 24.9)
+            {
+                lblClassificacao.Text = "Peso ideal (parabéns)";
+            }
+            else if (imc >= 25.0 && imc <= 29.9)
+            {
+                lblClassificacao.Text = "Levemente acima do peso";
+            }
+            else if (imc >= 30.0 && imc <= 34.9)
+            {
+                lblClassificacao.Text = "Obesidade grau I";
+            }
+            else if (imc >= 35.0 && imc <= 39.9)
+            {
+                lblClassificacao.Text = "Obesidade grau II (severa)";
+            }
+            else if (imc > 40.0)
+            {
+                lblClassificacao.Text = "Obesidade III (mórbida)";
+            }
+        }
+
+        private void txbPeso_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                if (((int)e.KeyChar) != ((int)Keys.Back))
+                    if (e.KeyChar != ',')
+                        e.Handled = true;
+                    else if (txbPeso.Text.IndexOf(',') > 0)
+                        e.Handled = true;
+            }
+        }
+
+        private void txbPeso_TextChanged(object sender, EventArgs e)
+        {
+            if (txbPeso.Text.Substring(0) == ",")
+                txbPeso.Text = "0" + txbPeso.Text;
+        }
+
+        private void txbAltura_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                if (((int)e.KeyChar) != ((int)Keys.Back))
+                    if (e.KeyChar != ',')
+                        e.Handled = true;
+                    else if (txbAltura.Text.IndexOf(',') > 0)
+                        e.Handled = true;
+            }
+        }
+
+        private void txbAltura_TextChanged(object sender, EventArgs e)
+        {
+            if (txbAltura.Text.Substring(0) == ",")
+                txbAltura.Text = "0" + txbAltura.Text;
+        }
+    }
+}
